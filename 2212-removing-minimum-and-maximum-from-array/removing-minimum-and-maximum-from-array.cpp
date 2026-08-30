c20fa0f138dd745c278664d1beac1a06bb3731c2
devanshbagre-boop <devanshbagre1@gmail.com>
2026-08-30 22:30:35 +0530

    Time: 0 ms (100.00%) | Memory: 92 MB (86.87%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.cpp
@@ -13,15 +13,8 @@ public:
         int left_idx = std::min(minatp, maxatp);
         int right_idx = std::max(minatp, maxatp);
 
-        // Option 1: Delete both from the left front
-        int op1 = right_idx + 1;
         
-        // Option 2: Delete both from the right back
-        int op2 = n - left_idx;
-        
-        // Option 3: Delete smaller from left, larger from right
-        int op3 = (left_idx + 1) + (n - right_idx);
 
-        return std::min({op1, op2, op3});
+        return min({right_idx + 1, n - left_idx, (left_idx + 1) + (n - right_idx)});
     }
 };
